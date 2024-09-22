d7c5235f92f32cba89b53dd0e2cfa32653593b6f
MarwanOmar99 <mrwan99.omar@gmail.com>
2024-09-22 21:09:52 +0300

    Change

--- src/Pages/Register/index.jsx
+++ src/Pages/Register/index.jsx
@@ -6,6 +6,7 @@ export default function Register() {
       <div className="row justify-content-center align-items-center rows">
         <div className="col-md-8 col-sm-10">
           <div className="form row justify-content-center align-items-center p-3 my-3 mx-2">
+            <p className="title">Create Account</p>
             <div className="col-md-6 col-sm-12 p-3">
               <label htmlFor="LoginEmail">Your Name</label>
               <input
@@ -34,7 +35,7 @@ export default function Register() {
               ></input>
             </div>
             <div className="col-md-6 col-sm-12 p-3">
-              <label htmlFor="LoginPass">Password</label>
+              <label htmlFor="LoginPass">Password Repeat</label>
               <input
                 placeholder="password"
                 type="password"
@@ -82,7 +83,7 @@ export default function Register() {
               </div>
             </div>
             <div className="OutForm text-center mt-2">
-              Don&apos;t have an account? <a>Register</a>
+              Already have an account? <a>Login</a>
             </div>
           </div>
         </div>
